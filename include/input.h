a216d75b88cacdd697abddef46f141c82407a6a6
nathanrun1 <nathanmreilly@gmail.com>
2026-01-28 14:42:40 -0800

    feat: basic window

---
+++ include/input.h
@@ -0,0 +1,8 @@
+﻿#ifndef WATERENGINE_INPUT_H
+#define WATERENGINE_INPUT_H
+
+namespace Input {
+    int numbaone();
+}
+
+#endif //WATERENGINE_INPUT_H
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#include "input.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    std::cout << "Hello" << std::endl;
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(400, 400, "yo", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+}
