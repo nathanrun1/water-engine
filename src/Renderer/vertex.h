1ff69035b955574d26cf842ba4fd0b86196f1ab7
nathanrun1 <nathanmreilly@gmail.com>
2026-01-30 10:22:33 -0800

    feat: Vertex class & texture coordinate support

---
+++ src/Renderer/vertex.h
@@ -0,0 +1,11 @@
+﻿#ifndef WATERENGINE_VERTEX_H
+#define WATERENGINE_VERTEX_H
+
+#include "glm/glm.hpp"
+
+struct Vertex {
+    glm::vec3 position;
+    glm::vec2 texCoord;
+};
+
+#endif //WATERENGINE_VERTEX_H
