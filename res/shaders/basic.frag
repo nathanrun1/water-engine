3a1e3fd9da304648586860788c7a66625d58c137
Nathaniel Reilly <n50055351@china.huawei.com>
2026-01-30 10:22:33 -0800

    feat: Vertex class & texture coordinate support

--- res/shaders/basic.frag
+++ res/shaders/basic.frag
@@ -1,7 +1,9 @@
 #version 460
 
+in vec2 texCoord;
+
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    fragColor = vec4(mix(vec2(0.0), vec2(1.0), texCoord), 0.5, 1.0);
 }
--- src/Renderer/renderer.cpp
+++ src/Renderer/renderer.cpp
@@ -1,13 +1,14 @@
 ﻿#include "renderer.h"
 
 #include "shader.h"
+#include "vertex.h"
 #include "Backend/glfw_backend.h"
 
-std::vector<float> vertices = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-     0.5f,  0.5f, 0.0f
+std::vector<Vertex> vertices = {
+    {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f}},
+    {{ 0.5f, -0.5f, 0.0f}, {1.0f, 0.0f}},
+    {{-0.5f,  0.5f, 0.0f}, {0.0f, 1.0f}},
+    {{ 0.5f,  0.5f, 0.0f}, {1.0f, 1.0f}}
 };
 
 std::vector<unsigned int> indices = {
@@ -31,7 +32,7 @@ void Renderer::init() {
 
     glGenBuffers(1, &VBO);       // Generates buffer, puts buffer ID into VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  // Binds our new VBO buffer to the GL_ARRAY_BUFFER target
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);  // Writes data to the buffer bound to GL_ARRAY_BUFFER (i.e. our VBO)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);  // Writes data to the buffer bound to GL_ARRAY_BUFFER (i.e. our VBO)
     // GL_STATIC_DRAW specifies STATIC (modified once, used many times) and DRAW (contents modified by application, used as source for drawing)
 
     glGenBuffers(1, &EBO);
@@ -40,8 +41,10 @@ void Renderer::init() {
 
     // Specify how vertex data is interpreted. For attr 0: 3 values of type float, non-normalized, with a stride of 3 * float size in memory, offset of 0 in VBO
     // Basically, will interpret each block of 3 values starting at every memory addr of (3 * sizeof(float)) byte stride starting from offset 0 in the array.
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)sizeof(Vertex::position));
     glEnableVertexAttribArray(0);  // Tell OpenGL shaders are allowed to use this attribute
+    glEnableVertexAttribArray(1);
 
     shaderProgram.use();
 }
